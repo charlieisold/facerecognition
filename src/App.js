1c805ccbe2cb3ab430ac2a463c0c026d4fb2c5f5
Mark Lang <marklangvt@gmail.com>
2020-07-10 13:40:56 -0400

    Add particle background, image link component, and rank component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,34 @@
 import React, { Component } from 'react';
-import Navigation from './components/navigation/navigation'
-import Logo from './components/logo/logo'
-
+import Particles from 'react-particles-js';
+import Navigation from './components/navigation/navigation';
+import Logo from './components/logo/logo';
+import ImageLinkForm from './components/imageLinkForm/imageLinkForm';
+import Rank from './components/rank/rank';
 import './App.css';
+
+const particlesOptions = {
+  particles: {
+      number: {
+        value: 30,
+        density: {
+          enable: true,
+          value_area: 800
+        }
+      }
+  }
+}
 
 class App extends Component {
   render() { 
     return (
       <div className="App">
+        <Particles className='particles'
+                params={particlesOptions} />
         <Navigation />
         <Logo />
-        {/* <ImageLinkForm />
-        <FaceRecognition /> */}
+        <Rank />
+        <ImageLinkForm />
+        {/* <FaceRecognition /> */}
       </div>
     );
   }
